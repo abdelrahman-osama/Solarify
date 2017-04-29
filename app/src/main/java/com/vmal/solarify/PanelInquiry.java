47c44c5cb459ac4bbee918b2aaecc5bf47b9a8cf
Abdelrahman Osama <abdelrahman.o.a@ieee.org>
2017-04-30 00:38:53 +0200

    xml edits

--- app/src/main/java/com/vmal/solarify/PanelInquiry.java
+++ app/src/main/java/com/vmal/solarify/PanelInquiry.java
@@ -3,12 +3,29 @@ package com.vmal.solarify;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.squareup.okhttp.Callback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PanelInquiry extends AppCompatActivity {
 
@@ -16,10 +33,14 @@ public class PanelInquiry extends AppCompatActivity {
     private ToggleButton noButton;
     private FloatingActionButton mNext;
 
+    private static final String TAG = "PanelInquiry";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_inquiry);
+
+        getPowerData();
 
         mNext = (FloatingActionButton) findViewById(R.id.next_button);
         yesButton = (ToggleButton) findViewById(R.id.student);
@@ -68,6 +89,66 @@ public class PanelInquiry extends AppCompatActivity {
                 }
             }
         });
+
+
+    }
+
+
+    void getPowerData() {
+
+        String BASE_URL = "https://developer.nrel.gov/api/pvwatts/v5.json?api_key=KhjqE5Ln7Ri9ckKXIej5onPadY3FDvCzlusgqPYB";
+
+        String lat = "30";
+        String lon = "30";
+        String systemCapacity = "4";
+        String azimuth = "180";
+        String tilt = "40";
+        String arrayType = "1";
+        String moduleType = "1";
+        String losses = "10";
+        String dataset = "intl";
+        String timeFrame = "hourly";
+
+        final String JSONRequest = BASE_URL + "&lat=" + lat + "&lon=" + lon + "&system_capacity=" + systemCapacity + "&azimuth=" + azimuth
+                + "&tilt=" + tilt + "&array_type=" + arrayType + "&module_type=" + moduleType + "&losses=" + losses + "&dataset=" + dataset + "&timeframe=" + timeFrame;
+
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(JSONRequest)
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    String responseString = response.body().string();
+                    Log.d("Response DATA:", response.body().string());
+                    try {
+                        JSONObject jsonData = new JSONObject(responseString);
+                        JSONObject testData = new JSONObject(jsonData.getString("outputs"));
+                        String testData2 = testData.getString("ac");
+                        Log.d("testData", testData2);
+
+                        String[] acData = testData2.split(",");
+                        Log.d("DataInStringArray", acData[13]);
+
+                        //TODO we should make an array that accepts the data coming from the JSON response
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+
+
+
+
 
 
     }
--- app/src/main/java/com/vmal/solarify/PowerForecastActivity.java
+++ app/src/main/java/com/vmal/solarify/PowerForecastActivity.java
@@ -9,5 +9,11 @@ public class PowerForecastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_forecast);
+
+
     }
+
+
+
+
 }
